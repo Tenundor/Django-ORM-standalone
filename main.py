848719c37f0ed90ebb6de3b9e01009eee1bf2477
Tenundor <71014004+Tenundor@users.noreply.github.com>
2020-11-18 18:40:20 +0300

    Task 2. Chose one pass

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ from datacenter.models import Passcard, Visit
 
 if __name__ == "__main__":
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())
+    passcards = Passcard.objects.all()
+    some_passcard = passcards[1]
+    print("Owner name:", some_passcard.owner_name)
+    print("Passcode:", some_passcard.passcode)
+    print("Created at:", some_passcard.created_at)
+    print("Is active:", some_passcard.is_active)
