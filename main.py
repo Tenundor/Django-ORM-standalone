8dbe6707f1ab80a67990560d7590b20f2bba9597
Tenundor <71014004+Tenundor@users.noreply.github.com>
2020-11-18 18:51:53 +0300

    Tasks 4, 5. Found all active passcards

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ from datacenter.models import Passcard, Visit
 if __name__ == "__main__":
     # Программируем здесь
     passcards = Passcard.objects.all()
-    some_passcard = passcards[1]
-    print("Owner name:", some_passcard.owner_name)
-    print("Passcode:", some_passcard.passcode)
-    print("Created at:", some_passcard.created_at)
-    print("Is active:", some_passcard.is_active)
+    active_passcards = Passcard.objects.filter(is_active=True)
+    print("Всего пропусков", len(passcards))
+    print("Активных пропусков", len(active_passcards))
