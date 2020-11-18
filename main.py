f928f3d8e1bbf9580d4a3680961542f227707bb5
Tenundor <71014004+Tenundor@users.noreply.github.com>
2020-11-18 21:41:11 +0300

    Task 9. Tried to solve the change tracking problem using the asyncio library

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import os
 import sys
 import django
+import asyncio
+from datacenter.models import Passcard, Visit
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 django.setup()
 
-from datacenter.models import Passcard, Visit
+
+async def print_visits_not_leaved():
+    visits_not_leaved = None
+    while True:
+        visits_not_leaved_updated = Visit.objects.filter(leaved_at__isnull=True)
+        if str(visits_not_leaved_updated) != str(visits_not_leaved):
+            print(visits_not_leaved_updated)
+            visits_not_leaved = visits_not_leaved_updated
+        await asyncio.sleep(1000)
 
 
 if __name__ == "__main__":
-    # Программируем здесь
-    visits_not_leaved = Visit.objects.filter(leaved_at__isnull=True)
-    print(visits_not_leaved)
+    asyncio.run(print_visits_not_leaved())
