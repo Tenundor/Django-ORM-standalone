d46b206b4977f3117f0a40562dbfcf0268ad66b3
Tenundor <71014004+Tenundor@users.noreply.github.com>
2020-11-19 20:36:35 +0300

    Task 11 completed

--- main.py
+++ main.py
@@ -11,33 +11,32 @@ django.setup()
 from datacenter.models import Passcard, Visit
 
 
-def calculate_storage_time(visit):
+def get_duration(visit):
     """
-    Returns the time spent by an employee in the storage in the format:
-
-    Находится в хранилище:
-    hh:mm:ss
+    Returns the time spent by an employee in the storage in seconds (int)
 
     Parameters
     ----------
     visit : <class 'datacenter.models.Visit'>
     """
+    entered_at_localtime = localtime(visit.entered_at)
+    current_localtime = localtime()
+    datetime_delta = current_localtime - entered_at_localtime
+
+    return int(datetime_delta.total_seconds())
 
 
 def seconds_to_str(seconds):
     mm, ss = divmod(seconds, 60)
     hh, mm = divmod(mm, 60)
-    return '{0:2d}:{1:2d}:{2:2d}'.format(hh, mm, ss)
+    return '{0:02d}:{1:02d}:{2:02d}'.format(hh, mm, ss)
 
 
 if __name__ == "__main__":
     visits_not_leaved = Visit.objects.filter(leaved_at__isnull=True)
-    for visit in visits_not_leaved:
-        print(visit.passcard)
-        entered_at_localtime = localtime(visit.entered_at)
-        print("Зашёл в хранилище, время по Москве:", entered_at_localtime, sep='\n')
-        current_localtime = localtime()
-        datetime_delta = current_localtime - entered_at_localtime
-        total_delta_seconds = int(datetime_delta.total_seconds())
-        time.sleep(5000)
-        print("\nНаходится в хранилище:", seconds_to_str(total_delta_seconds), sep='\n')
+    for visit_not_leaved in visits_not_leaved:
+        print(visit_not_leaved)
+        print("Зашёл в хранилище, время по Москве:", localtime(visit_not_leaved.entered_at), sep='\n')
+        time.sleep(10)
+        time_delta_seconds = get_duration(visit_not_leaved)
+        print("\nНаходится в хранилище:", seconds_to_str(time_delta_seconds), sep='\n')
